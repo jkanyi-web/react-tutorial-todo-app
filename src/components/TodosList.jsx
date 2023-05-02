992d5e7c19b8cb6df1e4025c4257ffc861d9ad62
kanyi <107926388+jkanyi-web@users.noreply.github.com>
2023-05-02 17:31:47 +0300

    reached part 4 of react tutorial

---
+++ src/components/TodosList.jsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import TodoItem from './TodoItem';
+
+const TodosList = ({
+  todosProps, handleChange, delTodo, setUpdate,
+}) => (
+  <ul>
+    {todosProps.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        itemProp={todo}
+        handleChange={handleChange}
+        delTodo={delTodo}
+        setUpdate={setUpdate}
+      />
+    ))}
+  </ul>
+);
+export default TodosList;
